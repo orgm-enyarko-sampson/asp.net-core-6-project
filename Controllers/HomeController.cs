9ffad47f8213decbe1e81522b74b5e534b00ae3e
orgm-enyarko-sampson <eric.nyarkosampson@orgmento.com>
2023-03-14 14:21:55 +0000

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers;
+
+public class HomeController : Controller
+{
+    public ViewResult Index()
+    {
+        return View("MyView");
+    }
+}
